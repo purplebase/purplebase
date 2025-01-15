6e7c86bc98a8a08b67b8fe8f70d3fcbd58d6476c
franzap <_@franzap.com>
2025-01-15 16:56:55 -0300

    Zap sender fallback from description event

--- lib/src/models/zap_receipt.dart
+++ lib/src/models/zap_receipt.dart
@@ -5,7 +5,10 @@ class ZapReceipt = RegularEvent<ZapReceipt> with ZapReceiptMixin;
 mixin ZapReceiptMixin on EventBase<ZapReceipt> {
   String get eventId => event.getTag('e')!;
   String get receiverPubkey => event.getTag('p')!;
-  String get senderPubkey => event.getTag('P')!;
+  Map<String, dynamic> get description => event.getTag('description') != null
+      ? Map<String, dynamic>.from(jsonDecode(event.getTag('description')!))
+      : {};
+  String get senderPubkey => event.getTag('P') ?? description['pubkey'];
 
   /// Amount in sats
   int get amount {
