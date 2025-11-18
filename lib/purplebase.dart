9b348dffc74048d3ad031a2355aab06d4463b2e8
franzap <_@franzap.com>
2025-11-18 19:58:24 -0300

    Fix exports

--- lib/purplebase.dart
+++ lib/purplebase.dart
@@ -4,7 +4,8 @@ export 'src/notifiers.dart';
 export 'src/isolate.dart'
     show RelayStatusMessage, PoolStateMessage, DebugMessage;
 export 'src/pool/state/pool_state.dart';
-export 'src/pool/state/subscription_phase.dart' show SubscriptionPhase;
+export 'src/pool/state/connection_phase.dart';
+export 'src/pool/state/subscription_phase.dart';
 export 'src/pool/publish/publish_response.dart' show PublishRelayResponse;
 export 'src/pool/websocket_pool.dart' show WebSocketPool;
 export 'src/request.dart';
